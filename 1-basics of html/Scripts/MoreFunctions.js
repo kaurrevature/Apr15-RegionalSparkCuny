b43a806fd39c991834ed613b2dd4685bc251ef87
Pushpinder Kaur <kaurrevature@outlook.com>
2019-04-23 14:24:22 -0400

    Functions and Algorithms

---
+++ 1-basics of html/Scripts/MoreFunctions.js
@@ -0,0 +1,134 @@
+//experiment with truthy and falsy
+function truthyFalsy(x) {
+    if (x) {
+        console.log(x + " is truthy");
+    } else {
+        console.log(x + " is falsy");
+    }
+}
+
+function largestOfThree(a, b, c) {
+    //check whether input is of type number
+    //evaluate whether the conversion to number fails for 
+    //ANY of the inputs
+    //if so, return NaN and exit the function
+    if (isNaN(Number(a)) || isNaN(Number(b)) || isNaN(Number(c))) {
+        return NaN; //exit function with value NaN
+    }
+    //ok, so now we can actually do the comparison!
+    //only way we can get to this point in the function
+    //is if the if-statement condition if not satisfied
+    //(could also use an else block)
+
+    //problem - what if a and b are equal and larger than c?
+    /*
+    if (a > b && a > c) {
+        return a;
+    } else if (b > a && b > c) {
+        return b;
+    } else {
+        return c;
+    }
+    */
+    //another approach
+    let largest = a;
+
+    if (b > largest) {
+        largest = b;
+    }
+    if (c > largest) {
+        largest = c;
+    }
+    //notice the pattern!
+    //moving through other values and comparing each one to 'largest'
+    //if current value is larger than 'largest', set it as 'largest'
+
+    return largest;
+}
+
+function largestInArray(numArray) {
+    let largest = numArray[0];
+    for (i = 1; i < numArray.length; i++) {
+        if (numArray[i] > largest) {
+            largest = numArray[i];
+        }
+    }
+    return largest;
+    //use a loop to find largest value
+}
+
+//without recursion
+function fibonacci(num){
+    var a = 1, b = 0, temp;
+  
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+  
+    return b;
+  }
+//with recursion
+function fibonacci(num) {
+    if (num <= 1) return 1;  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+
+console.log(fibonacci(2));
+
+function stringtoCharArr(str) {
+    debugger;
+    let char1 = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != ' ' && str[i] != " " && str[i] != ',' && str[i] != '.') {
+            char1.push(str[i].toLowerCase());
+        }
+    }
+    console.log(char1.toString());
+    return char1;
+}
+function reverseStringtoCharArr(str) {
+    debugger;
+    let char2 = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] != ' ' && str[i] != " " && str[i] != ',' && str[i] != '.') {
+            char2.push(str[i].toLowerCase());
+        }
+    }
+    console.log(char2.toString());
+    return char2;
+}
+function palindrome(str) {
+    let char1 = [];
+    let char2 = [];
+    isPalindrome=false;
+    falseCounter=0;
+    char1 = stringtoCharArr(str);
+    char2 = reverseStringtoCharArr(str);
+    debugger;
+    if (char1.length == char2.length) {
+        for (let i = 0; i < char1.length; i++) {
+            for (let j = 0; j < char2.length; j++) {
+                if(char1[i]==char2[j])
+                {
+                    isPalindrome=true;
+                }   
+                else {
+                    isPalindrome=false;
+                    falseCounter++;
+                }             
+            }
+            
+        }
+    }
+    if(false>0)
+    isPalindrome=false;    
+    return isPalindrome;
+}
+
+
+console.log(palindrome('A man, a plan, a canal. Panama'));
--- 1-basics of html/Scripts/function.js
+++ 1-basics of html/Scripts/function.js
@@ -4,36 +4,35 @@
 var a = 10;//GLOBAL Variables
 var b = 30;
 var result;
-function Add1() {  
+function Add1() {
     result = a + b;
     alert(result);
 }
 //Call Add1()
 //Add1();
 //2. with parameter and void type (alert for result)
-function Add2(x,y){
-    result=x+y;
+function Add2(x, y) {
+    result = x + y;
     alert(result);
 }
-function Add3(c,d){
-    result=c+d;
+function Add3(c, d) {
+    result = c + d;
     return result;
 }
-var output=Add3(70,8);
+var output = Add3(70, 8);
 //alert(output);
 //Add1();
 //Add2(a,b);
 //Add2(60,20);
-function takeNumber()
-{
-    var number=prompt("Enter the number to check if its even");
+function takeNumber() {
+    var number = prompt("Enter the number to check if its even");
     return number;
 }
 
 function isEvenOrOdd(takeNumber)//callback function
 {
-    var num=takeNumber();
-    if(num%2==0)
+    var num = takeNumber();
+    if (num % 2 == 0)
         return "even"
     else
         return "odd";
@@ -42,14 +41,14 @@ function isEvenOrOdd(takeNumber)//callback function
 
 //Recursion
 //Print reverse of numbers
-function printReverse(num){
+function printReverse(num) {
     //using recursion
 
     // document.write(num);
     // if(num>0)
     //     printReverse(num-1);
-debugger;
- //using while
+    debugger;
+    //using while
     // while(num>0){
     //     document.writeln(num);
     //     num--;
@@ -60,17 +59,16 @@ debugger;
     // }
     // while(num>0);
 
-    for(i=num;i<=num;i--)
-    {
+    for (i = num; i <= num; i--) {
         document.write(i);
-        if(i<=0)
+        if (i <= 0)
             break;
     }
 }
 //printReverse(5);
 //Print n natural numbers
-function printNaturalNumbers(limit){
-    for(var i=1;i<=limit;i++)//block/local scope
+function printNaturalNumbers(limit) {
+    for (var i = 1; i <= limit; i++)//block/local scope
     {
         //j=20; // global variable without var keyword
         //document.write(i);
@@ -92,6 +90,80 @@ function printNaturalNumbers(limit){
 //     alert('Hi Calling from anonymous function');
 // })();
 //anonymous and self invoked function with a parameter
-(function(a,b){
-    alert(`Sum of ${a} and ${b} is ${a+b}`);
-})(5,10);
+// (function(a,b){
+//     alert(`Sum of ${a} and ${b} is ${a+b}`);
+// })(5,10);
+
+var numArr = [10, 56, 76, 98, 82, 45, 98];
+function largestInArray(numArray) {
+    var largest = numArray[0];
+    for (i = 1; i < numArray.length; i++) {
+        if (numArray[i] > largest) {
+            largest = numArray[i];
+        }
+    }
+    return largest;
+    //use a loop to find largest value
+}
+
+var result = largestInArray(numArr);
+//alert(result);
+
+function stringtoCharArr(str) {
+    debugger;
+    let char1 = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != ' ' && str[i] != " " && str[i] != ',' && str[i] != '.') {
+            char1.push(str[i].toLowerCase());
+        }
+    }
+    console.log(char1.toString());
+    return char1;
+}
+function reverseStringtoCharArr(str) {
+    debugger;
+    let char2 = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] != ' ' && str[i] != " " && str[i] != ',' && str[i] != '.') {
+            char2.push(str[i].toLowerCase());
+        }
+    }
+    console.log(char2.toString());
+    return char2;
+}
+function palindrome(str) {
+    let char1 = [];
+    let char2 = [];
+    isPalindrome=false;
+    falseCounter=0;
+    char1 = stringtoCharArr(str);
+    char2 = reverseStringtoCharArr(str);
+    debugger;
+    if (char1.length == char2.length) {
+        for (let i = 0; i < char1.length; i++) {
+            for (let j = 0; j < char2.length; j++) {
+                if(char1[i]==char2[j])
+                {
+                    isPalindrome=true;
+                }   
+                else {
+                    isPalindrome=false;
+                    falseCounter++;
+                }             
+            }
+            
+        }
+    }
+    if(false>0)
+    isPalindrome=false;    
+    return isPalindrome;
+}
+
+
+console.log(palindrome('A man, a plan, a canal. Panama'));
+
+//https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+
+//https://medium.freecodecamp.org/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7
